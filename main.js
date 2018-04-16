89255678b7a7c5ba7600ec5a925ef8d581dd4f0e
lauralikespi <laura.gemmell012@gmail.com>
2018-04-16 09:58:09 +0100

    b

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const request = require('request');
 // const https = require('https');
-const fs = require('fs');
+// const fs = require('fs');
 const port = 8000;
 const app = express();
 
@@ -19,14 +19,13 @@ app.get('/', (req, res) => {
 app.get('/payment', (req, res) => {
   // const apexxUrl = 'https://sandbox.apexxfintech.com/v1/checkout';
   const apexxUrl = 'https://apexx.dimebox.com/v1/checkout';
-  // const API_KEY = 'GCKYSkERCH7XxFJVRLPjk_eMhxpyC70o537UMngjhME';
 
-  const API_KEY = 'TrWzfw0KQ8Z1-uaQsOsbMABb2UMIhR-MgRFMCG5Tfcc';
+  const API_KEY = "ENTER YOUR API KEY";
 
   var reqBody = {
-                  "account": "97a98914127313d929d7ae21",
+                  "account": "ENTER YOUR ACCOUNT",
                   "amount": 1750,
-                  "customer": "ba99ac87c437f5d5667288ea",
+                  "customer": "ENTER YOUR CUSTOMER",
                   "merchant_reference": "JJ-ORDER-0001",
                   "template_url": "https://lauralikespi.github.io/test.html",
                   "return_url": "https://jj7460.wixsite.com/visage",
